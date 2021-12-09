be09c3aee168ba1bda787a5517337bc6858d3ca6
Isabellanamutenda <namtendaisabella.com>
2021-12-09 22:13:31 +0300

    add fizzbuzz

--- solver.rb
+++ solver.rb
@@ -1,34 +1,27 @@
-# TDD Test Driven Development
 class Solver
-  def factorial(num)
-    if num.zero?
-      1
-    elsif num.positive?
-      num * factorial(num - 1)
-    else
-      raise ArgumentError, 'Only positive numbers are allowed'
+    def factorial(num)
+      if num.zero?
+        1
+      elsif num.positive?
+        num * factorial(num - 1)
+      else
+        'Oh, there is an error. Please use a positive number'
+      end
+    end
+  
+    def reverser(str)
+      str.reverse
+    end
+  
+    def fizzbuzz(num)
+      if num.modulo(3).zero? && num.modulo(5).zero?
+        'fizzbuzz'
+      elsif num.modulo(3).zero?
+        'fizz'
+      elsif num.modulo(5).zero?
+        'buzz'
+      else
+        num.to_s
+      end
     end
   end
-
-  def reverse(str)
-    if str.length.zero?
-      ''
-    else
-      str[-1] + reverse(str[0..-2])
-    end
-  end
-
-  def fizzbuzz(num)
-    if (num % 15).zero?
-      'FizzBuzz'
-    elsif (num % 3).zero?
-      'Fizz'
-    elsif (num % 5).zero?
-      'Buzz'
-
-      # returns the number when given a number not divisible by 3 or 5
-    elsif num % 3 != 0 && num % 5 != 0
-      num
-    end
-  end
-end
